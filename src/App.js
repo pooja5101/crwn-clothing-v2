b7774e3f227d852ba3122abb3563fc4004f84cea
Pooja Nehra <nehrap@rbos.co.uk>
2023-07-06 11:07:59 +0530

    changes done by pooja in navigation component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 
-import Home from './routes/home/home.componentt';
-i
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />} >
-         <Route index element={<Home />} />
+      <Route index element={<Home />} />
       </Route>
-      
-
     </Routes>
   );
 };
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,15 +1,24 @@
 import { Fragment } from "react";
+import { Outlet } from "react-router";
+import { Link } from "react-router-dom";
+
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+
+import './navigation.styles.scss';
 
 const Navigation = () => {
 
     return (
         <Fragment>
-            <div>Logo</div>
-            <div className="link-container">
-                <link to='/shop'>
-                    SHOP
-                </link>
+            <div>
+                <Logo/>
             </div>
+            <div className="nav-links-container">
+                <Link className='nav-link' to='/shop'>
+                    SHOP
+                </Link>
+            </div>
+            <Outlet/>
         </Fragment>
     );
 }
